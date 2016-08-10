9c2f755b772e9685836615c0fd12a6325f510f17
DjSkaarj <djskaarj99@gmail.com>
2016-08-10 21:09:42 +0300

    Обновлено меню, добавлены скины.
    Теперь всем скинам соответствует 1 картинка, а не несколько, как раньше.

--- src/menu.js
+++ src/menu.js
@@ -9,22 +9,32 @@ $(document).ready(function() {
     });
     
     $("#switch").click(function() {
+        $(".mleft-room-container").slideUp(500);
+            
         var state = $(".mleft-room-item").css("display");
         if(state == "none")
         {
+            $(this).find(".mleft-icon").css("background-image", "url(img/icon_toleft.png)");
+            
             $("#mleft").animate({
                 width: "300"
-            }, 500, function() {
-            });
-            $(".mleft-room-item").hide(0).delay(500).show(0);
+            }, 500);
+            $(".mleft-room-item").fadeOut(0).delay(300).fadeIn(200);
+            $(".mleft-button-title").fadeOut(0).delay(300).fadeIn(200)
         }
         else
         {
-            $(".mleft-room-item").hide();
+            $(this).find(".mleft-icon").css("background-image", "url(img/icon_toright.png)");
+            
+            $(this).find(".mleft-icon").animate({
+                backgroundColor: "red"
+            }, 500);
+            
+            $(".mleft-room-item").fadeOut(200);
+            $(".mleft-button-title").fadeOut(200);
             $("#mleft").animate({
-                width: "60"
-            }, 500, function() {
-            });
+                    width: "60"
+            }, 500);
         }
     });
 });
