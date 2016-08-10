aac416a8c3e032e2ea190f68b0a6ac35e099b3cd
DjSkaarj <djskaarj99@gmail.com>
2016-08-10 13:52:57 +0300

    Добавлены изображения AK-47 и M4A1

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
-    $(".mleft-room-item").click(function () {
-        $(this).find(".mleft-room-container").slideDown();
+    $(".mleft-room").click(function () {
+        var state = $(this).find(".mleft-room-container").css("display");
+        $(".mleft-room-container").slideUp();
+        if(state == "none")
+            $(this).find(".mleft-room-container").slideDown(500);
+        else 
+            $(this).find(".mleft-room-container").slideUp(500);
+    });
+    
+    $("#switch").click(function() {
+        var state = $(".mleft-room-item").css("display");
+        if(state == "none")
+        {
+            $("#mleft").animate({
+                width: "300"
+            }, 500, function() {
+            });
+            $(".mleft-room-item").hide(0).delay(500).show(0);
+        }
+        else
+        {
+            $(".mleft-room-item").hide();
+            $("#mleft").animate({
+                width: "60"
+            }, 500, function() {
+            });
+        }
     });
 });
